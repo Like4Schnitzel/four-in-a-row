5247bec19a9220197c3419ee9421fa33e5792f2a
Schnitzel <rw21062006@gmail.com>
2023-08-03 16:00:47 +0200

    implement winCheck method

--- src/board.cpp
+++ src/board.cpp
@@ -11,7 +11,16 @@ Board::Board()
     }
 }
 
-bool Board::setInColumn(int column, char player)
+char Board::getSlot(const int row, const int column)
+{
+    if (row > 5 || row < 0 || column > 6 || column < 0)
+        return '0';
+
+    //else
+    return board[row][column];
+}
+
+bool Board::setInColumn(const int column, const char player)
 {
     //check if something can be set in the column
     if (board[0][column] != '0') return false;
@@ -22,4 +31,37 @@ bool Board::setInColumn(int column, char player)
 
     board[lowest][column] = player;
     return true;
+}
+
+bool Board::checkLine(int x, int y, const int xDiff, const int yDiff, const char player)
+{
+    int count = 0;
+    for (int i = 0; i < 9; i++)
+    {
+        if (getSlot(x, y) == player)
+        {
+            count++;
+            if (count == 4)
+            {
+                return true;
+            }
+        }
+        else count = 0;
+
+        x += xDiff;
+        y += yDiff;
+    }
+
+    return false;
+}
+
+bool Board::winCheck(const int row, const int column)
+{
+    const char currentPlayer = board[row][column];
+    if (checkLine(row - 4, column - 4, 1, 1, currentPlayer) || //diagonal
+        checkLine(row - 4, column, 1, 0, currentPlayer) || //vertical
+        checkLine(row, column - 4, 0, 1, currentPlayer) ) // horizontal
+        return true;
+    //else
+    return false;
 }
--- src/board.hpp
+++ src/board.hpp
@@ -4,9 +4,11 @@ class Board {
     private:
         //6 rows, 7 columns
         char board[6][7];
+        char getSlot(int, int);
+        bool checkLine(int, int, int, int, char);
     public:
         Board();
         bool setInColumn(int, char);
-        bool winCheck();
+        bool winCheck(int, int);
         void printBoard();
 };
